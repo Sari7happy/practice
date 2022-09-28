f8a972a8fc3ae20bd22df50dac5c345e63b2e3a0
sari7happy <sari7happy@gmail.com>
2022-09-28 11:13:55 +0900

    ”バリデーションチェック”

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
-window.addEventListener('DOMContentLoaded',
-    function() {
-var node = document.getElementById('count-text');
-node.addEventListener('keyup',function(){
-        var count = this.value.length;
-        var counterNode = document.querySelector('.show-count-text');
-        counterNode.innerText = count;
+$(function(){
 
-        },false);
-    },false
-);
+const MSG_TEXT_MAX ='20字以内で入力してください。';
+const MSG_EMPTY ='入力必須です。';
+const MSG_EMIL_TYPE ='email形式ではありません。';
+const MSG_TEXTAREA_MAX ='100文字以内で入力してください。';
 
-// 大文字小文字気をつける。keyupはマウスを上げたら反応する
+$(".valid-text").keyup(function(){
+    var form_g =$(this).closest('form-group');
+    
+    if($(this).val().length>20){
+        form_g.removeClass('has-success').addClass('has-error');
+        form_g.find('.help-block').text('');
+
+    }
+});
+$(".valid-email").keyup(function(){
+
+
+    var form_g =$(this).closest('form-group');
+
+    if($(this).val().length === 0){
+        form_g.removeClass('has-success').addClass('has-error');
+        form_g.find('.help-block').text(MSG_EMPTY);
+
+    }else if ($(this).val().length >50 ||!$(this).val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/) ){
+        form_g.removeClass('has-success').addClass('has-error');
+        form_g.find('.help-block').text(MSG_EMIL_TYPE);
+    }else{
+        form_g.removeClass('has-error').addClass('has-success');
+        form_g.find('.help-block').text('');
+    }
+    });
+
+    $(".valid-textarea").keyup(function(){
+
+    var form_g = $(this).closest('.form-group');
+
+    if($(this).val().length === 0){
+        form_g.removeClass('has-success').addClass('has-error');
+        form_g.find('.help-block').text(MSG_EMPTY);
+    }else if( $(this).val().length > 100 ){
+        form_g.removeClass('has-success').addClass('has-error');
+        form_g.find('.help-block').text(MSG_TEXTAREA_MAX);
+    }else{
+        form_g.removeClass('has-error').addClass('has-success');
+        form_g.find('.help-block').text('');
+    }
+    });
+    
+    });
+
+
