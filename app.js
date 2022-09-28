19e1c298cb8db102607065cd9076afaba23e40d2
sari7happy <sari7happy@gmail.com>
2022-09-28 14:57:50 +0900

    ”JSの中身の意味”

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 $(function(){
+    // /function実行するよのサイン/ 
     const MSG_TEXT_MAX = '20文字以内で入力してください。';
     const MSG_EMPTY = '入力必須です。';
     const MSG_EMIL_TYPE = 'emailの形式ではありません。';
     const MSG_TEXTAREA_MAX = '100文字以内で入力してください。';
+    // 定数は大文字でかくエラーメッセージの内容
 
-$(".valid-text").keyup(function(){
+// 名前エリア
+    $(".valid-text").keyup(function(){
+        // クラスがvalid-textのもの
 
     var form_g = $(this).closest('.form-group');
+    // thisは名前フォームを指すclosest親の名前form-groupを指す
 
     if($(this) .val().length > 20){
+        // lengthは文字の長さ
         form_g.removeClass('has-success').addClass('has-error');
+        // ewmoveclassは削除、addclassは追加
         form_g.find('.help-block').text(MSG_TEXT_MAX);
+        // help-blockを探す、textを書き換えれるconstの中身を表示するMSG _TEXT _MAX
     }else{
         form_g.removeClass('has-error').addClass('has-success');
         form_g.find('.help-block').text('');
@@ -18,7 +26,7 @@ $(".valid-text").keyup(function(){
 
 });
 
-
+// EMAILエリア
 $(".valid-email").keyup(function(){
 
     var form_g = $(this) .closest('.form-group');
@@ -37,7 +45,7 @@ $(".valid-email").keyup(function(){
     }
 });
 
-
+// テキストエリア
 $(".valid-textarea").keyup(function(){
 
     var form_g = $(this).closest('.form-group');
